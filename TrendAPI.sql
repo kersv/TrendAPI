1cdc71099494c2c82fbcd4cc654baf6318fc054e
kersv <74420819+kersv@users.noreply.github.com>
2022-05-05 06:38:03 -0400

    Update TrendAPI.sql

--- TrendAPI.sql
+++ TrendAPI.sql
@@ -1,25 +1,21 @@
 CREATE DATABASE TrendMovieService;
 
-
 USE TrendMovieService;
 
 CREATE TABLE Trend(
 	TrendId INT NOT NULL AUTO_INCREMENT,
-    Genre VARCHAR(255) NOT NULL,
-    PRIMARY KEY (TrendId)
-
+    	Genre VARCHAR(255) NOT NULL,
+    	PRIMARY KEY (TrendId)
 );
     
 
 
 CREATE TABLE Movie(
 	MovieId INT NOT NULL AUTO_INCREMENT,
-    MovieName VARCHAR(255) NOT NULL,
-    MovieYear INT NOT NULL,
-    PRIMARY KEY (MovieId)
+    	MovieName VARCHAR(255) NOT NULL,
+    	MovieYear INT NOT NULL,
+        PRIMARY KEY (MovieId)
 );
-
-
 
 ALTER TABLE Trend ADD COLUMN MovieId INT;
 
@@ -27,16 +23,14 @@ ALTER TABLE Trend ADD FOREIGN KEY (MovieId) REFERENCES Movie(MovieId);
 
 ALTER TABLE Trend ADD CONSTRAINT FK_TrendMovie FOREIGN KEY (MovieId) REFERENCES Movie(MovieId);
 
-
-
 ALTER TABLE Movie ADD CONSTRAINT CHK_year CHECK (MovieYear >= 2000);
 
+ALTER TABLE Trend AUTO_INCREMENT = 1;
 
-INSERT INTO Movie (MovieName, MovieYear) VALUES ("example movie2", 2002);
-
-
-
-INSERT INTO Trend (Genre, MovieId) VALUES ("Horror", 1);
+INSERT INTO Movie (MovieName, MovieYear) VALUES ("Squid Game", 2021);
+INSERT INTO Trend (Genre, MovieId) VALUES ("Thriller", 1);
 
 SELECT * FROM Trend;
 SELECT * FROM Movie;
+
+
